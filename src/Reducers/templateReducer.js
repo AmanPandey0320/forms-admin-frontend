6d7263c99ba5e932c6eb19d3d41a88dfa2c24ec8
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-09-11 13:04:52 +0530

    delete from form reducer added in template

--- src/Reducers/templateReducer.js
+++ src/Reducers/templateReducer.js
@@ -10,6 +10,8 @@ const useReducer = (state = initState,action) => {
     }else if(action.type === 'SAVE_FORM'){
         const { id,form } = action;
         state.saved[`${id}`] = form;
+    }else if(action.type === 'DELETE_FROM_FORM'){
+        const { id, index } = action;
     }
     return state;
 }
